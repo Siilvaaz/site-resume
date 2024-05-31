9e0a978752001fd106d9cfe7b456177c0d28079c
Joao <jmvs98@gmail.com>
2024-05-31 18:02:50 +0100

    Created about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-
+import About from './components/About';
 function App() {
 
   return (
-    <>
-        Site em desenvolvimento. 
-    </>
+
+
+        <>
+            
+            <section id="about"><About/></section>
+            <section id="skills"></section>
+            <section id="projects"></section>
+            <section id="contact"></section>
+
+
+
+            <footer>
+              <p>Desenvolvido por <a href="https://www.linkedin.com/in/joao-mv-silva/" target="_blank">João Silva</a></p>
+            </footer>
+
+
+        </>
   )
 }
 
---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+        <h1>Sobre</h1>
+        <p>
+        Cloud Engineer with two years of solid experience in Google Cloud Platform
+        (GCP). During this time, I acquired comprehensive skills in the various services
+        offered by GCP, with a special focus on Kubernetes, Apigee and application
+        integration.
+
+        Actually in Master's Degree in Cybersecurity and Computer Forensics  at polytechnic of Leiria. 
+        
+
+        </p>
+        </div>
+    );
+    }
+
+    export default About;
